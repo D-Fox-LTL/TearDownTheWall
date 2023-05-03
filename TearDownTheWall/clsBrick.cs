f7af31c586f48fc4739d397e895ae1cc149c7800
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2023-05-03 09:54:37 +0200

    Add files via upload

---
+++ TearDownTheWall/clsBrick.cs
@@ -0,0 +1,89 @@
+﻿// ####################################################################################################################################
+//
+// Class of bricks
+// Created: 19.4.2023
+// Updated: 29.4.2023
+// Made by: D-Fox-LTL
+//
+// ####################################################################################################################################
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TearDownTheWall
+{
+    internal class clsBrick
+    {
+		//Variables
+
+		//Graphics
+		Graphics mobjGraphics;
+
+		// Ball variables
+		int mintXBrick, mintYBrick;
+		int mintWidthBrick, mintHeightBrick;
+		bool mblVisible;
+
+		//----------------------------
+		//Constructor
+		//----------------------------
+		public clsBrick(Graphics objCanvas, int intXBrick, int intYBrick, int intWidthBrick, int intHeightBrick)
+		{
+			mobjGraphics = objCanvas;
+
+			mintXBrick = intXBrick;
+			mintYBrick = intYBrick;
+			mintHeightBrick = intHeightBrick;
+			mintWidthBrick = intWidthBrick;
+			mblVisible = true;
+
+		}
+		//----------------------------
+		// Drawing of Bricks
+		//----------------------------
+		public void DrawBrick()
+		{
+			// if brick is visible
+			if (mblVisible == false) return;
+
+			//drawing of rectangle
+			mobjGraphics.FillRectangle(Brushes.Orange, mintXBrick, mintYBrick, mintWidthBrick, mintHeightBrick);
+		}
+        //----------------------------
+        // Test of brick/ball collision
+		// - returns True if the collision happened
+        //----------------------------
+
+
+		public bool TestOfCollision(int intXBall, int intYBall, int intWidthBall, int intHeightBall)
+		{
+			// if brick is visible
+			if (mblVisible == false) { return false; }
+
+
+			//defines ball and brick as a rectangles to compare
+			Rectangle ball = new Rectangle(intXBall, intYBall, intWidthBall, intHeightBall);
+			Rectangle brick = new Rectangle(mintXBrick, mintYBrick, mintWidthBrick, mintHeightBrick);
+
+
+            //test of collision
+            if (brick.IntersectsWith(ball))
+			{
+
+				mblVisible = false;
+
+                return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+    }
+}
---
+++ TearDownTheWall/clsRectangle.cs
@@ -0,0 +1,58 @@
+﻿// ####################################################################################################################################
+//
+// Class of ball
+// Created: 26.4.2023
+// Updated: 3.5.2023
+// Made by: D-Fox-LTL
+//
+// ####################################################################################################################################
+
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TearDownTheWall
+{
+    internal class clsRectangle
+    {
+		Graphics mobjGraphics;
+        //Rectangle
+        int mintXRectangle, mintYRectangle;
+        const int mintWidthOfRectangle = 120;
+        const int mintHeightOfRectangle = 20;
+
+        public clsRectangle(Graphics objCanvas)
+        {
+            mobjGraphics = objCanvas;
+            mintXRectangle = ((int)objCanvas.VisibleClipBounds.Width / 2) + (mintWidthOfRectangle/2);
+            mintYRectangle = (int)objCanvas.VisibleClipBounds.Height - 100;
+        }
+
+        public int intXRectangle
+        {
+            get { return mintXRectangle; }
+            set { mintXRectangle = value; }
+        }
+
+
+        public void drawRectangle()
+        {
+            mobjGraphics.FillRectangle(Brushes.Black, mintXRectangle, mintYRectangle, mintWidthOfRectangle, mintHeightOfRectangle);
+
+            //Collision on borders of pbCanvas
+            if (mintXRectangle + 120 >= (int)mobjGraphics.VisibleClipBounds.Height)
+            {
+                mintXRectangle = mintWidthOfRectangle - 5;
+            }
+            else if (mintXRectangle <= 0)
+            {
+               mintXRectangle = 5;
+            }
+        }
+	}
+}
