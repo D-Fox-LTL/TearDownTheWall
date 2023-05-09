c4b13bb76e30d33efee55fb830f2cf5bf13ef497
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2023-05-09 20:34:45 +0200

    Add files via upload

---
+++ TearDownTheWall/clsBall.cs
@@ -0,0 +1,168 @@
+﻿// ####################################################################################################################################
+//
+// Class of ball
+// Created: 19.4.2023
+// Updated: 6.5.2023
+// Made by: D-Fox-LTL
+//
+// ####################################################################################################################################
+
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TearDownTheWall
+{
+    public class clsBall
+    {
+        //Variables
+
+        //Graphics
+        Graphics mobjGraphics;
+
+        // Ball variables
+        int mintXBall, mintYBall;
+        int mintVectorX, mintVectorY;
+        const int mintRBall = 10;
+        int mintShiftSpeed;
+        bool mblGameOver = false;
+        int rndStart;
+        Random rndNumber = new Random();
+        
+
+        //----------------------------|
+        //        Constructor         |
+        //----------------------------|
+        public clsBall(Graphics objCanvas)
+        {
+            mobjGraphics = objCanvas;
+
+            mintXBall = (int)objCanvas.VisibleClipBounds.Width / 2;
+            mintYBall = (int)objCanvas.VisibleClipBounds.Height / 2;
+
+            mintShiftSpeed = 5;
+            rndStart = rndNumber.Next(1, 4);
+
+            mintVectorX = 0;
+            mintVectorY = 0;
+        }
+
+        //----------------------------|
+        //  load of ball coordinates  |
+        //----------------------------|
+
+        public int intXBall     //property
+        {
+            get { return mintXBall; }       //field
+        }
+        public int intYBall     //property
+        {
+            get { return mintYBall; }       //field
+        }
+        public int intRBall     //property
+        {
+            get { return mintRBall; }       //field
+        }
+        public bool blGameOver
+        {
+            get { return mblGameOver; }
+            set { mblGameOver = value; }
+        }
+
+
+
+        //----------------------------------------------------------------------|
+        //      changes ball's vector -> made  for canvas wall collisions       |
+        //----------------------------------------------------------------------|
+
+
+        public void vector()
+        {
+            //Drawing  of ball
+            mobjGraphics.FillEllipse(Brushes.Black, mintXBall, mintYBall, mintRBall, mintRBall);
+
+
+            //shift
+            mintXBall += mintVectorX;
+            mintYBall += mintVectorY;
+
+            //Collision on borders of pbCanvas
+            if (mintYBall <= 0)
+            {
+                mintVectorY = mintVectorY * (-1);
+            }
+            else if (mintXBall + mintRBall >= (int)mobjGraphics.VisibleClipBounds.Width || mintXBall <= 0)
+            {
+                mintVectorX = mintVectorX * (-1);
+            }
+            else if (mintYBall + mintRBall >= (int)mobjGraphics.VisibleClipBounds.Height)
+            {
+                //mintVectorY = mintVectorY * (-1);
+                mblGameOver = true;
+            }
+        }
+
+
+        //--------------------------------------------------------------------|
+        //      changes ball's vector -> made  for rectangle collisions       |
+        //--------------------------------------------------------------------|
+
+
+        public bool TestOfCollisionRectangle(int intXRectangle, int intYRectangle, int intWidthRectangle, int intHeightRectangle)
+        {
+
+            //defines ball and brick as a rectangles to compare
+            Rectangle rectangle = new Rectangle(intXRectangle, intYRectangle, intWidthRectangle, intHeightRectangle);
+            Rectangle rectBall = new Rectangle(mintXBall, mintYBall, mintRBall, mintRBall);
+
+
+            //test of collision
+            if (rectangle.IntersectsWith(rectBall))
+            {
+                //mintVectorY *= -1;
+                mintVectorY = rndNumber.Next(3, 8) * (-1);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+            //--------------------------------------------------------------------------|
+            //      changes ball's vector -> made especially for brick collisions       |
+            //--------------------------------------------------------------------------|
+
+            public void ChangeVector()
+        {
+            // void to change vectorY in the code
+            mintVectorY *= (-1);
+        }
+
+
+        //----------------------------|
+        //      starts the ball       |
+        //----------------------------|
+
+        public void startOfTheBall()
+        {
+            // Gives a speed to a ball through int given to vectors
+            if (rndStart == 1 || rndStart == 2)
+            {
+                mintVectorX = mintShiftSpeed;
+                mintVectorY = rndNumber.Next(2, 5);
+            } else
+            {
+                mintVectorX = mintShiftSpeed * (-1);
+                mintVectorY = rndNumber.Next(2, 5);
+            }
+            rndStart = rndNumber.Next(1, 4);
+        }
+    }
+}
---
+++ TearDownTheWall/clsRectangle.cs
@@ -0,0 +1,88 @@
+﻿// ####################################################################################################################################
+//
+// Class of ball
+// Created: 26.4.2023
+// Updated: 6.5.2023
+// Made by: D-Fox-LTL
+//
+// ####################################################################################################################################
+
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TearDownTheWall
+{
+    internal class clsRectangle
+    {
+		Graphics mobjGraphics;
+
+        //Rectangle
+        int mintXRectangle;
+        int mintYRectangle;
+        const int mintWidthOfRectangle = 90;
+        const int mintHeightOfRectangle = 10;
+
+
+        //----------------------------|
+        //        Constructor         |
+        //----------------------------|
+
+        public clsRectangle(Graphics objCanvas)
+        {
+            mobjGraphics = objCanvas;
+            mintYRectangle = (int)objCanvas.VisibleClipBounds.Height - 20;
+        }
+
+        //---------------------------------|
+        //  load of rectangle coordinates  |
+        //---------------------------------|
+
+        public int intXRectangle
+        {
+            get { return mintXRectangle; }
+            set { mintXRectangle = value; }
+        }   public int intWidthOfRectangle
+        {
+            get { return mintWidthOfRectangle; }
+        }   public int intHeightOfRectangle
+        {
+            get { return mintHeightOfRectangle; }
+        }   public int intYRectangle
+        {
+            get { return mintYRectangle; }
+        }
+
+
+
+        //--------------------------|
+        //      Draw rectangle      |
+        //--------------------------|
+
+
+        public void drawRectangle()
+        {
+            mobjGraphics.FillRectangle(Brushes.Black, mintXRectangle, mintYRectangle, mintWidthOfRectangle, mintHeightOfRectangle);
+
+            //Collision on borders of pbCanvas
+            if (mintXRectangle + mintWidthOfRectangle >= (int)mobjGraphics.VisibleClipBounds.Width)
+            {
+                mintXRectangle = (int)mobjGraphics.VisibleClipBounds.Width - 5 - mintWidthOfRectangle;
+            }
+            else if (mintXRectangle <= 0)
+            {
+               mintXRectangle = 5;
+            }
+        }
+
+        public void setRectangle()
+        {
+            mintXRectangle = (int)mobjGraphics.VisibleClipBounds.Width / 2 - (intWidthOfRectangle / 2);
+        }
+    }
+}
